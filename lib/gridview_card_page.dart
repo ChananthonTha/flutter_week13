71c0749ee2738f5f0bb10cf105402af97258a4e1
ชนันธร ถนอมเงิน <koonzee14@gmail.com>
2022-08-11 17:02:13 +0700

    mk2

--- lib/gridview_card_page.dart
+++ lib/gridview_card_page.dart
@@ -1,5 +1,19 @@
 import 'package:flutter/material.dart';
 
+class Menu{
+  final String? title;
+  final IconData? icon;
+  final Color? bkColor;
+
+  Menu({this.title,this.icon,this.bkColor});
+}
+
+List<Menu> menus = [
+  Menu(title: "MENU-1",icon:Icons.person, bkColor: Colors.orangeAccent),
+  Menu(title: "MENU-2",icon:Icons.person_add, bkColor: Colors.blue),
+  Menu(title: "MENU-3",icon:Icons.person_off, bkColor: Colors.red),
+];
+
 class GridViewPage extends StatelessWidget {
   const GridViewPage({Key? key}) : super(key: key);
 
@@ -10,36 +24,34 @@ class GridViewPage extends StatelessWidget {
       body: Container(
         padding: EdgeInsets.all(8),
         child: GridView.count(
-          crossAxisCount: 3,
-          mainAxisSpacing: 20,
-          crossAxisSpacing: 20,
-          // children: List.generate(6, (index) => Card()),
-          children: List.generate(9, (index) {
-            return Card(
-              color: Colors.orangeAccent,
-              elevation: 5,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.all(
-                  Radius.circular(20)
+            crossAxisCount: menus.length,
+            mainAxisSpacing: 20,
+            crossAxisSpacing: 20,
+            // children: List.generate(9, (index) => Card()),
+            children: List.generate(menus.length, (index) {
+              return Card(
+                color: menus[index].bkColor,
+                elevation: 5,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(
+                        Radius.circular(20)
+                    )
                 ),
-              ),
-              child: InkWell(
-                borderRadius: BorderRadius.all(
-                  Radius.circular(20)
+                child: InkWell(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(20),
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(menus[index].icon,size: 65,color: Colors.white),
+                      Text(menus[index].title!,style: TextStyle(color: Colors.white))
+                    ],
+                  ),
+                  onTap: () {},
                 ),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Icon(Icons.person,size: 65,color: Colors.white,),
-                    Text("Menu-1")
-                  ],
-                ),
-                onTap: () {
-
-                },
-              ),
-            );
-          }),
+              );
+            })
         ),
       ),
     );
